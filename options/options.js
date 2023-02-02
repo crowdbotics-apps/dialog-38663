da56ee7bfd8dc2312b9bd98e8dbaef03332d096f
Crowdbotics <team@crowdbotics.com>
2023-02-02 16:05:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dialog-38663",
+  url: "https://dialog-38663.botics.co",
+  api: "https://dialog-38663.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
